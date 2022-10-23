34a9255616c15a550515fe77922e6fea8db8f001
Daniel Carmichael <danielfcarmichael@gmail.com>
2022-10-23 03:37:33 -0700

    Added male data

--- Generators/_class_person.js
+++ Generators/_class_person.js
@@ -7,8 +7,133 @@ class Person {
     }
 }
 
+function generatePersonality() {
+    const randFloat = Math.random() * 101;
+    var statistical_personality = "";
+
+        if ( randFloat <= 2.1 && randFloat > 0 ) {
+            statistical_personality = "intj";
+        }
+        else if ( randFloat <= 5.4 && randFloat > 2.1 ) {
+            statistical_personality = "intp";
+        }
+        else if ( randFloat <= 7.2 && randFloat > 5.4 ) {
+            statistical_personality = "entj";
+        }
+        else if ( randFloat <= 10.4 && randFloat > 7.2 ) {
+            statistical_personality = "entp";
+        }
+        else if ( randFloat <= 11.9 && randFloat > 10.4 ) {
+            statistical_personality = "infj";
+        }
+        else if ( randFloat <= 16.3 && randFloat > 11.9 ) {
+            statistical_personality = "infp";
+        }
+        else if ( randFloat <= 24.4 && randFloat > 16.3 ) {
+            statistical_personality = "enfp";
+        }
+        else if ( randFloat <= 36 && randFloat > 24.4 ) {
+            statistical_personality = "istj";
+        }
+        else if ( randFloat <= 49.8 && randFloat >36 ) {
+            statistical_personality = "isfj";
+        }
+        else if ( randFloat <= 58.5 && randFloat > 49.8 ) {
+            statistical_personality = "estj";
+        }
+        else if ( randFloat <= 70.5 && randFloat > 58.5 ) {
+            statistical_personality = "esfj";
+        }
+        else if ( randFloat <= 75.9 && randFloat > 70.5 ) {
+            statistical_personality = "istp";
+        }
+        else if ( randFloat <= 84.7 && randFloat > 75.9 ) {
+            statistical_personality = "isfp";
+        }
+        else if ( randFloat <= 89 && randFloat > 84.7 ) {
+            statistical_personality = "estp";
+        }
+        else if ( randFloat <= 97.5 && randFloat > 89 ) {
+            statistical_personality = "esfp";
+        }
+        else if ( randFloat <= 100.0 && randFloat > 97.5 ) {
+            statistical_personality = "enfj";
+        }
+
+    return statistical_personality;
+}
+
+function generateTraitsMale() {
+
+    const randHair = Math.random() * 101;
+    const randHeight = Math.random() * 101;
+    var hair_color = "";
+    var height = 0;
+
+    // calculate hair
+    if ( randHair <= 85 && randHair > 0 ) {
+        hair_color = "black";
+    }
+    else if ( randHair <= 96 && randHair > 85 ) {
+        hair_color = "brown";
+    }
+    else if ( randHair <= 98 && randHair > 96 ) {
+        hair_color = "blonde";
+    }
+    else if ( randHair <= 98 && randHair > 99 ) {
+        hair_color = "red";
+    }
+    else if ( randHair <= 100 && randHair > 99 ) {
+        hair_color = "white";
+    }
+
+    // calculate height
+    if ( randHeight <= 1 && randHeight > 0 ) {
+        height = 63;
+    }
+    else if ( randHeight <= 4 && randHeight > 1 ) {
+        height = 64;
+    }
+    else if ( randHeight <= 8 && randHeight > 4 ) {
+        height = 65;
+    }
+    else if ( randHeight <= 14 && randHeight > 8 ) {
+        height = 66;
+    }
+    else if ( randHeight <= 21 && randHeight > 14 ) {
+        height = 67;
+    }
+    else if ( randHeight <= 33 && randHeight > 21 ) {
+        height = 68;
+    }
+    else if ( randHeight <= 50 && randHeight > 33 ) {
+        height = 69;
+    }
+    else if ( randHeight <= 67 && randHeight > 50 ) {
+        height = 70;
+    }
+    else if ( randHeight <= 79 && randHeight > 97 ) {
+        height = 71;
+    }
+    else if ( randHeight <= 86 && randHeight > 79 ) {
+        height = 72;
+    }
+    else if ( randHeight <= 92 && randHeight > 86 ) {
+        height = 73;
+    }
+    else if ( randHeight <= 96 && randHeight > 92 ) {
+        height = 74;
+    }
+    else if ( randHeight <= 99 && randHeight > 96 ) {
+        height = 75;
+    }
+    else if ( randHeight <= 100 && randHeight > 99 ) {
+        height = 76;
+    }
+}
+
 const digiSapien = new Person(
-    personalities.intj,
+    generatePersonality(),
     {
         "hair": "blonde",
         "skin_color": "white"
--- Personalities/_data_personalities.js
+++ Personalities/_data_personalities.js
@@ -1,13 +1,7 @@
 exports.personalities = {
     "intj": {
+        "statical_advantage": 2.1,
         "title": "Architect",
-        "attributes": [
-            "imaginative",
-            "strategic",
-            "thinker",
-            "planner",
-            "pioneering"
-        ],
         "personality_description": "It can be lonely at the top. As one of the rarest personality types – and one of the most capable – Architects (INTJs) know this all too well. Rational and quick-witted, Architects pride themselves on their ability to think for themselves, not to mention their uncanny knack for seeing right through phoniness and hypocrisy. But because their minds are never at rest, Architects may struggle to find people who can keep up with their nonstop analysis of everything around them.",
         "strengths": [
             "rational",
@@ -23,22 +17,11 @@ exports.personalities = {
             "overly critical",
             "combative",
             "socially clueless"
-        ],
-        "romanctic_values": [
-            "depth",
-            "intelligence",
-            "unmitigated honesty"
         ]
     },
     "intp": {
+        "statical_advantage": 3.3,
         "title": "Logician",
-        "attributes": [
-            "analytical",
-            "imaginative",
-            "thinker",
-            "perceptive",
-            "creative"
-        ],
         "personality_description": "Logicians pride themselves on their unique perspectives and vigorous intellect. They can’t help but puzzle over the mysteries of the universe – which may explain why some of the most influential philosophers and scientists of all time have been Logicians. This personality type is fairly rare, but with their creativity and inventiveness, Logicians aren’t afraid to stand out from the crowd.",
         "strengths": [
             "analytical",
@@ -53,22 +36,11 @@ exports.personalities = {
             "dissatisfied",
             "impatient",
             "perfectionistic"
-        ],
-        "romanctic_values": [
-            "genuinity",
-            "complex",
-            "direct"
         ]
     },
     "entj": {
+        "statical_advantage": 1.8,
         "title": "Commander",
-        "attributes": [
-            "leader",
-            "strategic",
-            "thinker",
-            "precise",
-            "determined"
-        ],
         "personality_description": "Commanders are natural-born leaders. People with this personality type embody the gifts of charisma and confidence, and project authority in a way that draws crowds together behind a common goal. However, Commanders are also characterized by an often ruthless level of rationality, using their drive, determination and sharp minds to achieve whatever end they’ve set for themselves. Perhaps it is best that they make up only three percent of the population, lest they overwhelm the more timid and sensitive personality types that make up much of the rest of the world – but we have Commanders to thank for many of the businesses and institutions we take for granted every day.",
         "strengths": [
             "efficient",
@@ -85,22 +57,11 @@ exports.personalities = {
             "arrogant",
             "poor handling of emotions",
             "cold and ruthless"
-        ],
-        "romanctic_values": [
-            "moves first",
-            "determined",
-            "long term"
         ]
     },
     "entp": {
+        "statical_advantage": 3.2,
         "title": "Debater",
-        "attributes": [
-            "rebellious",
-            "contentious",
-            "thinker",
-            "clever",
-            "quick witted"
-        ],
         "personality_description": "Quick-witted and audacious, Debaters aren’t afraid to disagree with the status quo. In fact, they’re not afraid to disagree with pretty much anything or anyone. Few things light up people with this personality type more than a bit of verbal sparring – and if the conversation veers into controversial terrain, so much the better.",
         "strengths": [
             "knowledgeable",
@@ -116,11 +77,258 @@ exports.personalities = {
             "insensitive",
             "difficulty focusing",
             "dislikes practical matters"
+        ]
+    },
+    "infj": {
+        "statical_advantage": 1.5,
+        "title": "Advocate",
+        "personality_description": "Advocates (INFJs) may be the rarest personality type of all, but they certainly leave their mark on the world. Idealistic and principled, they aren’t content to coast through life – they want to stand up and make a difference. For Advocate personalities, success doesn’t come from money or status but from seeking fulfillment, helping others, and being a force for good in the world.",
+        "strengths": [
+            "creative",
+            "insightful",
+            "principled",
+            "passionate",
+            "altruistic"
         ],
-        "romanctic_values": [
-            "intentional",
-            "vigor",
-            "enthusiastic"
+        "weaknesses": [
+            "sensitive",
+            "reluctant to adventure",
+            "perfectionistic",
+            "avoiding the ordinary",
+            "prone to burnout"
+        ]
+    },
+    "infp": {
+        "statical_advantage": 4.4,
+        "title": "Mediator",
+        "personality_description": "Although they may seem quiet or unassuming, Mediators (INFPs) have vibrant, passionate inner lives. Creative and imaginative, they happily lose themselves in daydreams, inventing all sorts of stories and conversations in their minds. These personalities are known for their sensitivity – Mediators can have profound emotional responses to music, art, nature, and the people around them.",
+        "strengths": [
+            "empathetic",
+            "generous",
+            "open-minded",
+            "creative",
+            "passionate",
+            "idealistic"
+        ],
+        "weaknesses": [
+            "unrealistic",
+            "self isolating",
+            "unfocused",
+            "emotionally vulnerable",
+            "desparate to please",
+            "self critical"
+        ]
+    },
+    "enfj": {
+        "statical_advantage": 2.5,
+        "title": "Protagonist",
+        "personality_description": "Protagonists (ENFJs) feel called to serve a greater purpose in life. Thoughtful and idealistic, these personality types strive to have a positive impact on other people and the world around them. They rarely shy away from an opportunity to do the right thing, even when doing so is far from easy.",
+        "strengths": [
+            "receptive",
+            "reliable",
+            "passionate",
+            "altruistic",
+            "charismatic"
+        ],
+        "weaknesses": [
+            "unrealistic",
+            "overly idealistic",
+            "condescending",
+            "intense",
+            "overly empathetic"
+        ]
+    },
+    "enfp": {
+        "statical_advantage": 8.1,
+        "title": "Campaigner",
+        "personality_description": "Campaigners (ENFPs) are true free spirits – outgoing, openhearted, and open-minded. With their lively, upbeat approach to life, they stand out in any crowd. But even though they can be the life of the party, Campaigners don’t just care about having a good time. These personality types run deep – as does their longing for meaningful, emotional connections with other people.",
+        "strengths": [
+            "curious",
+            "perceptive",
+            "enthusiastic",
+            "excellent communicators",
+            "festive",
+            "good-natured"
+        ],
+        "weaknesses": [
+            "people-pleasing",
+            "unfocused",
+            "disorganized",
+            "overly accomidating",
+            "overly optimistic",
+            "restless"
+        ]
+    },
+    "istj": {
+        "statical_advantage": 11.6,
+        "title": "Logistician",
+        "personality_description": "Logisticians pride themselves on their integrity. People with this personality type mean what they say, and when they commit to doing something, they make sure to follow through.",
+        "strengths": [
+            "honest and direct",
+            "strong-willed and dutiful",
+            "very responsible",
+            "calm and practical",
+            "create and enforce order",
+            "jack-of-all-trades"
+        ],
+        "weaknesses": [
+            "stubborn",
+            "insensitive",
+            "always by the book",
+            "judgemental",
+            "often unreasonably blame themselves"
+        ]
+    },
+    "isfj": {
+        "statical_advantage": 13.8,
+        "title": "Defender",
+        "personality_description": "In their unassuming, understated way, Defenders help make the world go round. Hardworking and devoted, people with this personality type feel a deep sense of responsibility to those around them. Defenders can be counted on to meet deadlines, remember birthdays and special occasions, uphold traditions, and shower their loved ones with gestures of care and support. But they rarely demand recognition for all that they do, preferring instead to operate behind the scenes.",
+        "strengths": [
+            "supportive",
+            "reliable",
+            "observant",
+            "enthusiastic",
+            "hard working",
+            "good practical skills"
+        ],
+        "weaknesses": [
+            "overly humble",
+            "taking things personally",
+            "repressing their feelings",
+            "overcomitted",
+            "reluctant to change",
+            "too altruistic"
+        ]
+    },
+    "estj": {
+        "statical_advantage": 8.7,
+        "title": "Executive",
+        "attributes": [
+            "rebellious",
+            "contentious",
+            "thinker",
+            "clever",
+            "quick witted"
+        ],
+        "personality_description": "Executives are representatives of tradition and order, utilizing their understanding of what is right, wrong and socially acceptable to bring families and communities together. Embracing the values of honesty, dedication and dignity, people with the Executive personality type are valued for their clear advice and guidance, and they happily lead the way on difficult paths. Taking pride in bringing people together, Executives often take on roles as community organizers, working hard to bring everyone together in celebration of cherished local events, or in defense of the traditional values that hold families and communities together.",
+        "strengths": [
+            "dedicated",
+            "strong-willed",
+            "direct and honest",
+            "loyal, patient and reliable",
+            "enjoys creating order",
+            "excellent organizer"
+        ],
+        "weaknesses": [
+            "inflexible and stubborn",
+            "uncomfortable with unconventional situations",
+            "judgemental",
+            "too focused on social status",
+            "difficult to relax",
+            "difficulty expressing emotions"
+        ]
+    },
+    "esfj": {
+        "statical_advantage": 12,
+        "title": "Consul",
+        "personality_description": "For Consuls, life is sweetest when it’s shared with others. People with this personality type form the bedrock of many communities, opening their homes – and their hearts – to friends, loved ones, and neighbors.",
+        "strengths": [
+            "stropng practical skills",
+            "strong sense of duty",
+            "very loyal",
+            "sensitive and warm",
+            "good at connecting with others"
+        ],
+        "weaknesses": [
+            "worried about their social status",
+            "inflexible",
+            "reluctant to innovate",
+            "vulnerable to criticism",
+            "often too needy",
+            "too selfless"
+        ]
+    },
+    "istp": {
+        "statical_advantage": 5.4,
+        "title": "Virtuoso",
+        "personality_description": "Virtuosos love to explore with their hands and their eyes, touching and examining the world around them with cool rationalism and spirited curiosity. People with this personality type are natural Makers, moving from project to project, building the useful and the superfluous for the fun of it, and learning from their environment as they go. Often mechanics and engineers, Virtuosos find no greater joy than in getting their hands dirty pulling things apart and putting them back together, just a little bit better than they were before.",
+        "strengths": [
+            "optimistic and energetic",
+            "quick thinker",
+            "creative and practical",
+            "spntaneuous and rational",
+            "knows how to prioritize",
+            "great in a crisis",
+            "relaxed"
+        ],
+        "weaknesses": [
+            "stubborn",
+            "insensitive",
+            "private and reserved",
+            "easily bored",
+            "dislikes commitment",
+            "risky behavior"
+        ]
+    },
+    "isfp": {
+        "statical_advantage": 8.8,
+        "title": "Adventurer",
+        "personality_description": "Adventurers are true artists – although not necessarily in the conventional sense. For this personality type, life itself is a canvas for self-expression. From what they wear to how they spend their free time, Adventurers act in ways that vividly reflect who they are as unique individuals.",
+        "strengths": [
+            "charming",
+            "sensitive",
+            "imaginative",
+            "passionate",
+            "curious",
+            "artistic"
+        ],
+        "weaknesses": [
+            "fiercly independent",
+            "unpredictable",
+            "easily stressed",
+            "overly competative",
+            "fluctuating self-esteem"
+        ]
+    },
+    "estp": {
+        "statical_advantage": 4.3,
+        "title": "Entrepreneur",
+        "personality_description": "Entrepreneurs always have an impact on their immediate surroundings – the best way to spot them at a party is to look for the whirling eddy of people flitting about them as they move from group to group. Laughing and entertaining with a blunt and earthy humor, Entrepreneur personalities love to be the center of attention. If an audience member is asked to come on stage, Entrepreneurs volunteer – or volunteer a shy friend.",
+        "strengths": [
+            "bold",
+            "rational and practical",
+            "original",
+            "perceptive",
+            "direct",
+            "sociable"
+        ],
+        "weaknesses": [
+            "insensitve",
+            "impatient",
+            "risk-prone",
+            "unstructured",
+            "may miss the bigger picture",
+            "defiant"
+        ]
+    },
+    "esfp": {
+        "statical_advantage": 8.5,
+        "title": "Entertainer",
+        "personality_description": "If anyone is to be found spontaneously breaking into song and dance, it is the Entertainer personality type. Entertainers get caught up in the excitement of the moment, and want everyone else to feel that way, too. No other personality type is as generous with their time and energy as Entertainers when it comes to encouraging others, and no other personality type does it with such irresistible style.",
+        "strengths": [
+            "bold",
+            "original",
+            "aesthetics and showmanship",
+            "practical",
+            "observant",
+            "excellent people skills"
+        ],
+        "weaknesses": [
+            "sensitive",
+            "conflict averse",
+            "easily bored",
+            "poor long term planner",
+            "unfocused"
         ]
     }
 };
